0ab100b4a36d1352e25da2cd33b494dadcdaf49d
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-06 22:12:34 +0300

    ResultStatus oluşturuldu.

--- ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
+++ ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 
 namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
 {

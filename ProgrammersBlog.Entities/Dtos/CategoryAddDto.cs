900071965d3c3a32164ad9a4f7dcb0b5e547ce62
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-07 02:21:21 +0300

    CategoryAddDto alanı tamanlandı.

--- ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
+++ ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,24 @@ namespace ProgrammersBlog.Entities.Dtos
 {
     public class CategoryAddDto
     {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} Adı boş geçilemez.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz.")]
+        public string Name { get; set; }
+
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz.")]
+        public string Description { get; set; }
+
+        [DisplayName("Kategori özel not alanı")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz.")]
+        public string Note { get; set; }
+
+        [DisplayName("Aktif mi?")]
+        [Required(ErrorMessage = "{0} Adı boş geçilemez.")]
+        public bool IsActive { get; set; }
     }
 }

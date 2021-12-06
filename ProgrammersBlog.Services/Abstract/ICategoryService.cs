bee58b285df6cc838899a4604d86648a12911bb6
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-07 01:49:13 +0300

    CategoryAddDto and CategoryUpdateDto boş oluşturuldu.

--- ProgrammersBlog.Services/Abstract/ICategoryService.cs
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
 using ProgrammersBlog.Shared.Utilities.Results.Abstract;
 
 namespace ProgrammersBlog.Services.Abstract

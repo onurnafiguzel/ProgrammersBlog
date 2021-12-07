ddee54f5fb43742e160ed3e0366ce1e438b837f7
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-07 13:21:46 +0300

    ArticleDto, ArticleListDto ve DtoGetBase sınıfları tamamlandı.

--- ProgrammersBlog.Entities/Dtos/ArticleDto.cs
+++ ProgrammersBlog.Entities/Dtos/ArticleDto.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 
 namespace ProgrammersBlog.Entities.Dtos
 {
-    public class ArticleDto
+    public class ArticleDto : DtoGetBase
     {
+        public Article Article { get; set; }
+        // public override ResultStatus ResultStatus { get; set; } = ResultStatus.Success;
     }
 }
--- ProgrammersBlog.Entities/Dtos/ArticleListDto.cs
+++ ProgrammersBlog.Entities/Dtos/ArticleListDto.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 
 namespace ProgrammersBlog.Entities.Dtos
 {
-    public class ArticleListDto
+    public class ArticleListDto : DtoGetBase
     {
+        public IList<Article> Articles { get; set; }
     }
 }

3df6c2d004175662fce1ff5963216d43dc7ff01f
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-12-09 00:56:37 +0300

    Migration daha sonra yapılacak.

--- ProgrammersBlog.Entities/Concrete/Article.cs
+++ ProgrammersBlog.Entities/Concrete/Article.cs
@@ -15,7 +15,7 @@ namespace ProgrammersBlog.Entities.Concrete
         public string Content { get; set; }
         public string Thumbnail { get; set; }
         public DateTime Date { get; set; }
-        public int ViewCount { get; set; } = 0;
+        public int ViewsCount { get; set; } = 0;
         public int CommentCount { get; set; } = 0;
         public string SeoAuthor { get; set; }
         public string SeoDescription { get; set; }
